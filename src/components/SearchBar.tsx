13360235b9c458f5c17d2977d008e770be956e8d
Muhammad Siddiqui <s760132@gmail.com>
2024-05-24 05:19:26 -0500

    implement Navbar Icons and Modals

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -18,7 +18,7 @@ const SearchBar = () => {
 
   return (
     <form
-      className="flex ic justify-between gap-4 bg-gray-100 p-2 rounded-md flex-1"
+      className="flex items-center justify-between gap-4 bg-gray-100 p-2 rounded-md flex-1"
       onSubmit={handleSearch}
     >
       <input
