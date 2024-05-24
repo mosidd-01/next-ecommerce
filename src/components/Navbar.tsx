8bcfc8099332d314f79b65734a976f3e46bb5476
Muhammad Siddiqui <s760132@gmail.com>
2024-05-24 04:40:07 -0500

    Create pages and layouts, create responsive navbar, create search input

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,37 @@
+import Link from "next/link";
+import React from "react";
+import Menu from "./Menu";
+import Image from "next/image";
+import SearchBar from "./SearchBar";
+import NavIcons from "./NavIcons";
+
+const Navbar = () => {
+  return (
+    <div className="h-20 px-4 md:px-8 lg:px-16 2xl:px-64 relative">
+      {/* Mobile */}
+      <div className="h-full flex items-center justify-between md:hidden">
+        <Link href="/">
+          <div className="text-2xl tracking-wide">LAMA</div>
+        </Link>
+        <Menu />
+      </div>
+      {/* Bigger screen */}
+      <div className="hidden md:flex items-center justify-between gap-8 h-full">
+        {/* Left */}
+        <div className="w-1/3">
+          <Link href="/" className="flex items-center gap-3">
+            <Image src="/logo.png" alt="logo" width={24} height={24} />
+            <div className="text-2xl tracking-wide">LAMA</div>
+          </Link>
+        </div>
+        {/* Right */}
+        <div className="w-2/3 flex items-center justify-between gap-8">
+          <SearchBar />
+          <NavIcons />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
