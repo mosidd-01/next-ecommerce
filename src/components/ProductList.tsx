69c733a53c1090443e671298678d5cd48850613e
Muhammad Siddiqui <s760132@gmail.com>
2024-05-27 14:48:05 -0500

    implement Category List Design

--- src/components/ProductList.tsx
+++ src/components/ProductList.tsx
@@ -91,6 +91,35 @@ const ProductList = () => {
           Add to Cart
         </button>
       </Link>
+      <Link
+        href="/test"
+        className="w-full flex flex-col gap-4 sm:w-[45%] lg:w-[22%]"
+      >
+        <div className="relative w-full h-80">
+          <Image
+            src="https://images.pexels.com/photos/24280095/pexels-photo-24280095/free-photo-of-an-armchair-and-a-basket-of-flowers-standing-in-a-garden.jpeg?auto=compress&cs=tinysrgb&w=800&lazy=load"
+            alt=""
+            fill
+            sizes="25vw"
+            className="absolute object-cover rounded-md z-10 hover:opacity-0 transition-opacity easy duration-500"
+          />
+          <Image
+            src="https://images.pexels.com/photos/18413376/pexels-photo-18413376/free-photo-of-pink-roses-sticking-in-water-at-a-shore.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
+            alt=""
+            fill
+            sizes="25vw"
+            className="absolute object-cover rounded-md"
+          />
+        </div>
+        <div className="flex justify-between">
+          <span className="font-medium">Product Name</span>
+          <span className="font-semibold">$49</span>
+        </div>
+        <div className="text-sm text-gray-500">My Description</div>
+        <button className="rounded-2xl ring-1 ring-coolred text-coolred w-max py-2 px-4 text-xs hover:bg-coolred hover:text-white">
+          Add to Cart
+        </button>
+      </Link>
     </div>
   );
 };
