629822b653f297624ae210969ae0751159e718da
Muhammad Siddiqui <s760132@gmail.com>
2024-06-02 21:14:43 -0500

    Fetch categories data

--- src/app/list/page.tsx
+++ src/app/list/page.tsx
@@ -1,8 +1,19 @@
 import Filter from "@/components/Filter";
 import ProductList from "@/components/ProductList";
+import Skeleton from "@/components/Skeleton";
+import { wixClientServer } from "@/lib/wixClientServer";
 import Image from "next/image";
+import { Suspense } from "react";
 
-const ListPage = () => {
+const ListPage = async ({ searchParams }: { searchParams: any }) => {
+  const wixClient = await wixClientServer();
+
+  const cat = await wixClient.collections.getCollectionBySlug(
+    searchParams.cat || "all-products"
+  );
+
+  //console.log(cat);
+
   return (
     <div className="px-4 md:px-8 lg:px-16 2xl:px-64 relative">
       {/* CAMPAIGN */}
@@ -24,7 +35,14 @@ const ListPage = () => {
       <Filter />
       {/* PRODUCTS */}
       <h1 className="mt-12 text-xl font-semibold">Shoes For You</h1>
-      <ProductList />
+      <Suspense fallback={<Skeleton />}>
+        <ProductList
+          categoryId={
+            cat.collection?._id || "00000000-000000-000000-000000000001"
+          }
+          searchParams={searchParams}
+        />
+      </Suspense>
     </div>
   );
 };
