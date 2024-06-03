b5a859d724af075f92f6869281d44b71af1694c4
Muhammad Siddiqui <s760132@gmail.com>
2024-06-02 21:28:22 -0500

    Fetch single page data

--- src/app/[slug]/page.tsx
+++ src/app/[slug]/page.tsx
@@ -1,59 +1,58 @@
 import Add from "@/components/Add";
 import CustomizeProducts from "@/components/CustomizeProducts";
 import ProductImages from "@/components/ProductImages";
+import { wixClientServer } from "@/lib/wixClientServer";
+import { notFound } from "next/navigation";
 
-const SinglePage = () => {
+const SinglePage = async ({ params }: { params: { slug: string } }) => {
+  const wixClient = await wixClientServer();
+
+  const products = await wixClient.products
+    .queryProducts()
+    .eq("slug", params.slug)
+    .find();
+
+  if (!products.items[0]) {
+    return notFound();
+  }
+
+  const product = products.items[0];
+
   return (
     <div className="px-4 md:px-8 lg:px-16 2xl:px-64 relative flex flex-col lg:flex-row gap-16">
       {/* IMG */}
       <div className="w-full lg:w-1/2 lg:sticky top-20 h-max">
-        <ProductImages />
+        <ProductImages items={product.media?.items} />
       </div>
       {/* TEXTS */}
       <div className="w-full lg:w-1/2 flex flex-col gap-6">
-        <h1 className="text-4xl font-medium">Product Name</h1>
-        <p className="text-gray-500">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat
-          deleniti facilis fuga expedita voluptate veritatis rerum provident
-          molestiae repudiandae consectetur nisi rem sed, pariatur assumenda
-          odio eligendi? Ex, iusto harum.
-        </p>
+        <h1 className="text-4xl font-medium">{product.name}</h1>
+        <p className="text-gray-500">{product.description}</p>
         <div className="h-[2px] bg-gray-100" />
-        <div className="flex items-center gap-4">
-          <h3 className="text-xl text-gray-500 line-through">$59</h3>
-          <h2 className="font-medium text-2xl">$49</h2>
-        </div>
+
+        {product.price?.price === product.price?.discountedPrice ? (
+          <h2 className="font-medium text-2xl">${product.price?.price}</h2>
+        ) : (
+          <div className="flex items-center gap-4">
+            <h3 className="text-xl text-gray-500 line-through">
+              ${product.price?.price}
+            </h3>
+            <h2 className="font-medium text-2xl">
+              ${product.price?.discountedPrice}
+            </h2>
+          </div>
+        )}
+
         <div className="h-[2px] bg-gray-100" />
         <CustomizeProducts />
         <Add />
         <div className="h-[2px] bg-gray-100" />
-        <div className="text-sm">
-          <h4 className="font-medium mb-4">Title</h4>
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias
-            doloremque expedita in, quasi maiores voluptas exercitationem eaque
-            voluptatibus harum neque repellendus, minima tenetur id inventore!
-            Vero possimus qui exercitationem aliquam?
-          </p>
-        </div>
-        <div className="text-sm">
-          <h4 className="font-medium mb-4">Title</h4>
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias
-            doloremque expedita in, quasi maiores voluptas exercitationem eaque
-            voluptatibus harum neque repellendus, minima tenetur id inventore!
-            Vero possimus qui exercitationem aliquam?
-          </p>
-        </div>
-        <div className="text-sm">
-          <h4 className="font-medium mb-4">Title</h4>
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias
-            doloremque expedita in, quasi maiores voluptas exercitationem eaque
-            voluptatibus harum neque repellendus, minima tenetur id inventore!
-            Vero possimus qui exercitationem aliquam?
-          </p>
-        </div>
+        {product.additionalInfoSections?.map((section: any) => (
+          <div className="text-sm" key={section.title}>
+            <h4 className="font-medium mb-4">{section.title}</h4>
+            <p>{section.description}</p>
+          </div>
+        ))}
       </div>
     </div>
   );
