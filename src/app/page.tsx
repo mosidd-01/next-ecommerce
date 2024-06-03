238dff61bd9f6267481a862958d2b8a85a8123ec
Muhammad Siddiqui <s760132@gmail.com>
2024-06-02 19:29:21 -0500

    Add server side data fetching

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,23 +1,29 @@
-"use client";
+//"use client";
 
 import CategoryList from "@/components/CategoryList";
 import ProductList from "@/components/ProductList";
 import Slider from "@/components/Slider";
 import { WixClientContext } from "@/context/wixContext";
+import { useWixClient } from "@/hooks/useWixClient";
+import { wixClientServer } from "@/lib/wixClientServer";
 import { useContext, useEffect } from "react";
 
-const HomePage = () => {
-  const wixClient = useContext(WixClientContext);
+const HomePage = async () => {
+  // const wixClient = useWixClient();
 
-  useEffect(() => {
-    const getProducts = async () => {
-      const res = await wixClient.products.queryProducts().find();
+  // useEffect(() => {
+  //   const getProducts = async () => {
+  //     const res = await wixClient.products.queryProducts().find();
 
-      console.log(res);
-    };
+  //     console.log(res);
+  //   };
 
-    getProducts();
-  }, [wixClient]);
+  //   getProducts();
+  // }, [wixClient]);
+
+  const wixClient = await wixClientServer();
+  const res = await wixClient.products.queryProducts().find();
+  console.log(res);
 
   return (
     <div className="">
